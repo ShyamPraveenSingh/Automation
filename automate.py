7453d185520b90b197370497d06edd3e8529f2c8
ShyamPraveenSingh <shyampraveensingh@gmail.com>
2020-07-20 20:03:17 +0530

    version 1

---
+++ automate.py
@@ -0,0 +1,19 @@
+#Program to open Google Chrome and search given keyword.
+#AUTOMATION
+
+#import the pyautogui module
+import pyautogui
+
+#pyautogui.position())   #get the position of the mouse
+# pos: chrome - 698,767
+pyautogui.click(698, 767, interval = 2)
+pyautogui.typewrite("facebook.com", interval=0.3)
+pyautogui.typewrite(["enter"])
+
+
+#pyautogui.click(1337, 7)
+
+
+
+
+#P.S. All position are subjected to change, these positions are valid only in my PC
